5b7da315a137a150ff55f5210ac309b21dcd4d2f
Track <659956865@qq.com>
2019-11-20 15:31:48 +0800

    增加啊登录页中v-model

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -38,6 +38,12 @@ export default {
     components: {
         bililHeader,
         [Input.name]: Input,
+    },
+    data(){
+        return {
+            account: '',
+            password: ''
+        }
     }
 }
 </script>
