60a39cba441ac46e64c29aae2906a184e1ccdeae
Track <659956865@qq.com>
2020-02-28 13:44:26 +0800

    feat:完成video

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -9,16 +9,45 @@
                         <span>{{this.videoTitle}}</span>
                     </div>
                 </div>
+                <!-- 左边整个页面 -->
                 <div class="play-wrapp">
+                    <!-- 头部 -->
                     <div class="play-wrapp-top"></div>
-                    <div class="play-wrapp-video">
-                        <video  ref="video" id="video" :src="`http://localhost/barrage-video-website-serve/public/video/${this.videoPath}`"></video>
+                    <!-- 播放器 -->
+                    <div class="play-wrapp-video" @click="pause()">
+                        <video  ref="video"  :src="`http://localhost/barrage-video-website-serve/public/video/${this.videoPath}`"></video>
                     </div>
+                    <!-- 低层 -->
                     <div class="player-video-buttom">
-                        <div class="player-video-control-top">
+                        <div class="player-video-control-top" @mousedown="videoSeek">
                             <!-- 进度条 -->
-                            <div class="play-video-progress" id="play-video-progress" ref="playVideoProgress" @mousedown="videoSeek" v-bind:style="{width:percent + '%'}">
+                            <div class="play-video-progress" id="play-video-progress" ref="playVideoProgress"  v-bind:style="{width:percent + '%'}">
                                 <div class="bui-track" id="bui-track"  ref="buiTrack" ></div>
+                            </div>
+                        </div>
+                        <!-- 进度条底下控件 -->
+                        <div class="player-video-control-main">
+                            <!-- 播放暂停,记录当前时间,总时间 -->
+                            <div class="buttom-left">
+                                <div class="video-pause"></div>
+                                <div class="video-time">
+                                    <!-- new Date(endDate ) -->
+                                    <span>{{this.comThisCurrentTime}}</span>
+                                    <span>/</span>
+                                    <span>{{this.comThisDurationTime}}</span>
+                                </div>
+                            </div>
+                            <div class="buttom-right">
+                                <div class="buttom-music button-container" title="音量">
+                                    <div class="music-progress-container">
+                                        <div class="music-num">{{this.musicPercent}}</div>
+                                        <div class="music-progress-wrap" @mousedown="musicSeek" ref="musicProgressWrap">
+                                            <div class="music-progress" ref="musicProgress" v-bind:style="{height:musicPercent + '%'}"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="button-container" >自动</div>
+                                <div class="buttom-full-screen button-container" title="全屏" @click="fullscreen"></div>
                             </div>
                         </div>
                     </div>
@@ -43,7 +72,7 @@ import apiPrefix from '@/api/core/apiPrefix.js'
 import { Message } from 'element-ui'
 import bililHeader from '@/components/header.vue'
 export default {
-    name: 'video',
+    name: 'videoLayout',
     components: {
         bililHeader
     },
@@ -54,6 +83,7 @@ export default {
             videoPath: '',
             videoTitle: '',
             percent: this.percentProeecss,
+            musicPercent: 100,
             duration: 0,
             currentTime: 0
         }
@@ -65,6 +95,34 @@ export default {
     computed: {
         percentProeecss(){
             return (this.currentTime / this.duration) * 100
+        },
+        comThisCurrentTime(){
+            const s = this.currentTime
+            var day = Math.floor(s / (24 * 3600))
+            var hour = Math.floor((s - day * 24 * 3600) / 3600)
+            var minute = Math.floor((s - day * 24 * 3600 - hour * 3600) / 60)
+            var second = Math.floor(s - day * 24 * 3600 - hour * 3600 - minute * 60)
+            if(minute < 10){
+                minute = '0' + minute
+            }
+            if(second < 10){
+                second = '0' + second
+            }
+            return minute + ':' + second
+        },
+        comThisDurationTime(){
+            const s = this.duration
+            var day = Math.floor(s / (24 * 3600))
+            var hour = Math.floor((s - day * 24 * 3600) / 3600)
+            var minute = Math.floor((s - day * 24 * 3600 - hour * 3600) / 60)
+            var second = Math.floor(s - day * 24 * 3600 - hour * 3600 - minute * 60)
+            if(minute < 10){
+                minute = '0' + minute
+            }
+            if(second < 10){
+                second = '0' + second
+            }
+            return minute + ':' + second
         }
     },
     watch: {
@@ -92,7 +150,10 @@ export default {
             })
         },
         changVideoTime(currentTime = 0){
-            this.currentTime = currentTime
+            if(currentTime !== 0){
+                this.currentTime = currentTime
+            }
+
             // 60帧数
             setInterval(()=>{
                 this.currentTime = this.$refs.video.currentTime
@@ -117,6 +178,23 @@ export default {
             video.currentTime = percent * video.duration
             this.percent = percent * 100
             this.changVideoTime()
+        },
+        musicSeek(e){
+            var video = this.$refs.video
+            var musicProgress = this.$refs.musicProgress
+            var musicProgressWrap = this.$refs.musicProgressWrap
+            var length = e.clientY - 630 - musicProgress.offsetHeight
+            var percent = length / musicProgressWrap.offsetHeight
+            percent = Math.floor(percent * 100)
+            if(percent > 100){
+                percent = 100
+            }
+            this.musicPercent = percent
+            video.preventDefault()
+            video.volume = percent / 100
+        },
+        fullscreen(){
+            this.$refs.video.webkitRequestFullScreen()
         }
     }
 }
@@ -170,6 +248,7 @@ export default {
             height: 48px;
         }
         .play-wrapp-video{
+            cursor: pointer;
             video{
                 width: 964px;
                 height: 614px;
@@ -188,7 +267,6 @@ export default {
             display: flex;
             justify-content: center;
             align-items: center;
-            cursor: pointer;
             .player-sig-container{
                 position: relative;
             }
@@ -205,6 +283,7 @@ export default {
             }
         }
     }
+    // 进度条
     .player-video-buttom{
         width: 100%;
         height: 50px;
@@ -215,6 +294,7 @@ export default {
         padding: 0 12px;
             .player-video-control-top{
                 transition:  opacity .2s ease-in;
+                cursor: pointer;
                 opacity: 1;
                 height: 16px;
                 .play-video-progress{
@@ -229,5 +309,92 @@ export default {
                     }
                 }
             }
+    }
+    // 进度条下面
+    .player-video-control-main{
+        width: 100%;
+        height: 35px;
+        .buttom-left{
+            height: 100%;
+            float: left;
+            .video-pause{
+                cursor: pointer;
+                width: 0;
+                border-top: 10px solid transparent;
+                border-right: 10px solid transparent;
+                border-bottom: 10px solid transparent;
+                border-left: 10px solid white;
+                border-radius: 4px;
+                margin-left: 10px;
+                margin-right: 10px;
+                display: inline-block;
+                vertical-align: top;
+            }
+            .video-time{
+                display: inline-block;
+                width: 90px;
+                height: 22px;
+                color: #fff
+            }
+        }
+        .buttom-right{
+            height: 100%;
+            float: right;
+            display: flex;
+            color: #fff;
+            .button-container{
+                margin-right: 10px;
+                font-size: 15px;
+                height: 22px;
+                width: 30px;
+                cursor: pointer;
+            }
+            .buttom-full-screen{
+                background-image: url('../pictures/icons.png');
+                background-position-x: -276px;
+                background-position-y: -918px;
+            }
+            .buttom-music{
+                background-image: url('../pictures/icons.png');
+                background-position-x: -666px;
+                background-position-y: -1686px;
+                position: relative;
+                &:hover{
+                    .music-progress-container{
+                        display: block;
+                    }
+                }
+                .music-progress-container{
+                    width: 36px;
+                    height: 100px;
+                    position: absolute;
+                    left: -10px;
+                    top: -124px;
+                    background: #104;
+                    color: #fff;
+                    display: none;
+                    .music-num{
+                        display: flex;
+                        justify-content: center;
+                    }
+                    .music-progress-wrap{
+                        transform: rotate(180deg);
+                        transform-origin:50 50%;
+                        position: absolute;
+                        left: 15px;
+                        top: 23px;
+                        line-height: 70px;
+                        text-align: center;
+                        width: 4px;
+                        height: 70px;
+                        background: white;
+                    }
+                    .music-progress{
+                        width: 100%;
+                        background:blue;
+                    }
+                }
+            }
+        }
     }
 </style>
