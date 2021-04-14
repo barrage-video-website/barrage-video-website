e6fd80455a712e17f0df98024cff8c5461ad4faf
Track <659956865@qq.com>
2021-04-14 17:15:12 +0800

    refactor:重构直播

--- src/views/Livebroadcast.vue
+++ src/views/Livebroadcast.vue
@@ -12,35 +12,8 @@
                 <div class="play-wrapp">
                     <div class="play-wrapp-top"></div>
                     <div class="play-wrapp-video">
-                        <video id="video" ref="video"></video>
-                    </div>
-                    <!-- 低层 -->
-                    <div class="player-video-buttom">
-                        <div class="player-video-control-top" >
-                            <!-- 进度条 -->
-                            <div class="play-video-progress" id="play-video-progress" ref="playVideoProgress"  >
-                                <div class="bui-track" id="bui-track"  ref="buiTrack" ></div>
-                            </div>
-                        </div>
-                        <!-- 进度条底下控件 -->
-                        <div class="player-video-control-main">
-                            <!-- 播放暂停,记录当前时间,总时间 -->
-                            <div class="buttom-left">
-                                <div class="video-pause"></div>
-                            </div>
-                            <div class="buttom-right">
-                                <div class="buttom-music button-container" title="音量">
-                                    <div class="music-progress-container">
-                                        <div class="music-num"></div>
-                                        <div class="music-progress-wrap" ref="musicProgressWrap">
-                                            <div class="music-progress" ref="musicProgress"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="button-container" >自动</div>
-                                <div class="buttom-full-screen button-container" title="全屏" @click="fullscreen"></div>
-                            </div>
-                        </div>
+                        <d-player class="video" ref="dplayer" :options="options"></d-player>
+                        <!-- <video id="video" ref="video"></video> -->
                     </div>
                 </div>
           </div>
@@ -52,15 +25,52 @@
 <script>
 import bililHeader from '@/components/header.vue'
 import flvjs from 'flv.js'
+import VueDPlayer from 'vue-dplayer'
 export default {
     name: 'live',
     components: {
-        bililHeader
+        bililHeader,
+        'd-player': VueDPlayer
+    },
+    data(){
+        return{
+            dp: null,
+            options: {
+                container: this.$refs.dplayer,
+                live: true,
+                video: {
+                    url: 'http://192.168.145.128:8080/live?port=9999&app=myapp&stream=mystream',
+                    type: 'customFlv',
+                    customType: {
+                        customFlv: function (video, player) {
+                            const flvPlayer = flvjs.createPlayer({
+                                type: 'flv',
+                                url: video.src,
+                                fixAudioTimestampGap: false
+                            })
+                            flvPlayer.attachMediaElement(video)
+                            flvPlayer.load()
+                        }
+                    }
+                }
+                // danmaku: {
+                //     id: '9E2E3368B56CDBB4',
+                //     api: 'https://api.prprpr.me/dplayer/',
+                //     // token: 'tokendemo',
+                //     maximum: 1000,
+                //     bottom: '25%',
+                //     unlimited: true
+                // }
+            }
+        }
     },
     mounted(){
-        setTimeout(()=>{
-            this.load()
-        }, 2000)
+        // setTimeout(()=>{
+        //     this.load()
+        // }, 2000)
+        this.dp = this.$refs.dplayer.dp
+        // this.load()
+        // console.log(this.dp)
     },
     methods: {
         load(){
@@ -102,6 +112,10 @@ export default {
 </script>
 
 <style lang="less">
+    // .video{
+    //     width: 100%;
+    //     height: 100%;
+    // }
     #bilil-header{
         box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
     }
@@ -140,8 +154,8 @@ export default {
         }
     }
      .play-wrapp{
-        width: 100%;
-        height: 676px;
+        // width: 100%;
+        // height: 676px;
         background: black;
         position: relative;
         .play-wrapp-top{
@@ -151,151 +165,9 @@ export default {
         .play-wrapp-video{
             cursor: pointer;
             video{
-                width: 964px;
-                height: 614px;
             }
 
         }
-        // 暂停符号
-        .player-pause{
-            width: 64px;
-            height: 64px;
-            background: white;
-            position: absolute;
-            right: 34px;
-            bottom: 70px;
-            border-radius: 8px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            .player-sig-container{
-                position: relative;
-            }
-            .player-sig{
-                position: absolute;
-                top: -15px;
-                left: -10px;
-                width: 0;
-                border-top: 20px solid transparent;
-                border-right: 20px solid transparent;
-                border-bottom: 20px solid transparent;
-                border-left: 20px solid black;
+    }
 
-            }
-        }
-    }
-    // 进度条
-    .player-video-buttom{
-        width: 100%;
-        height: 50px;
-        position: absolute;
-        left: 0;
-        bottom: 0;
-        box-sizing: border-box;
-        padding: 0 12px;
-            .player-video-control-top{
-                transition:  opacity .2s ease-in;
-                cursor: pointer;
-                opacity: 1;
-                height: 16px;
-                .play-video-progress{
-                    width: 0;
-                    height: 4px !important;
-                    transition: all .5;
-                    background: hsla(0,0%,100%,.2);
-                    .bui-track{
-                        width: 940px;
-                        background: hsla(0,0%,100%,.3);
-                        height: 4px ;
-                    }
-                }
-            }
-    }
-    // 进度条下面
-    .player-video-control-main{
-        width: 100%;
-        height: 35px;
-        .buttom-left{
-            height: 100%;
-            float: left;
-            .video-pause{
-                cursor: pointer;
-                width: 0;
-                border-top: 10px solid transparent;
-                border-right: 10px solid transparent;
-                border-bottom: 10px solid transparent;
-                border-left: 10px solid white;
-                border-radius: 4px;
-                margin-left: 10px;
-                margin-right: 10px;
-                display: inline-block;
-                vertical-align: top;
-            }
-            .video-time{
-                display: inline-block;
-                width: 90px;
-                height: 22px;
-                color: #fff
-            }
-        }
-        .buttom-right{
-            height: 100%;
-            float: right;
-            display: flex;
-            color: #fff;
-            .button-container{
-                margin-right: 10px;
-                font-size: 15px;
-                height: 22px;
-                width: 30px;
-                cursor: pointer;
-            }
-            .buttom-full-screen{
-                background-image: url('../pictures/icons.png');
-                background-position-x: -276px;
-                background-position-y: -918px;
-            }
-            .buttom-music{
-                background-image: url('../pictures/icons.png');
-                background-position-x: -666px;
-                background-position-y: -1686px;
-                position: relative;
-                &:hover{
-                    .music-progress-container{
-                        display: block;
-                    }
-                }
-                .music-progress-container{
-                    width: 36px;
-                    height: 100px;
-                    position: absolute;
-                    left: -10px;
-                    top: -124px;
-                    background: #104;
-                    color: #fff;
-                    display: none;
-                    .music-num{
-                        display: flex;
-                        justify-content: center;
-                    }
-                    .music-progress-wrap{
-                        transform: rotate(180deg);
-                        transform-origin:50 50%;
-                        position: absolute;
-                        left: 15px;
-                        top: 23px;
-                        line-height: 70px;
-                        text-align: center;
-                        width: 4px;
-                        height: 70px;
-                        background: white;
-                    }
-                    .music-progress{
-                        width: 100%;
-                        background:blue;
-                    }
-                }
-            }
-        }
-    }
 </style>
